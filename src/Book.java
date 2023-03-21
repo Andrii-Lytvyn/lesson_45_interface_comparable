a4f9ff20ee5887af697773fe9129a041bf6ab89d
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-03-21 22:48:38 +0100

    Add class Book

---
+++ src/Book.java
@@ -0,0 +1,19 @@
+public class Book {
+
+    /*
+Задача 1
+Создайте класс Book (книга), в конструктор которого передавайте автора, название книги
+ и количество страниц.
+Реализуйте интерфейс для сравнения книг: они должны сортироваться по авторам,
+ а если авторы совпадают - по названиям (и там, и там - по алфавиту, "в словарном порядке").*/
+
+  String author;
+  String title;
+  int pages;
+
+  public Book(String author, String title, int pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+  }
+}
