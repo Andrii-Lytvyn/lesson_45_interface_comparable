c9c7520e5d125d4eac3acdd133c1506c8363f528
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-04-14 14:52:52 +0200

    Add SortByTitle lambda

--- src/Main.java
+++ src/Main.java
@@ -9,35 +9,54 @@ public class Main {
  Создайте список книг и отсортируйте книги по названию, (при одинаковых названиях - по автору) при помощи лямбда-функции.
   */
 
-  public static void main(String[] args) {
-    List<Book> bookList = new ArrayList<>();
-    bookList.add(new Book(" 'Ayn Rand' ", " 'Abckedabra' ", 1140));
-    bookList.add(new Book(" 'Ayn Rand' ", " 'Atlas Shrugged' ", 1140));
-    bookList.add(new Book(" 'Rowling' ", " 'Harry Potter' ", 260));
-    bookList.add(new Book(" 'Rowling' ", " 'Abckedabra' ", 11));
-    for (Book books : bookList) {
-      System.out.println(books);
-    }
+    public static void main(String[] args) {
+        List<Book> bookList = new ArrayList<>();
 
-    System.out.println("Sort by author:");
-    Collections.sort(bookList);
-    for (Book books : bookList) {
-      System.out.println(books);
-    }
+        bookList.add(new Book(" 'Ayn Rand' ", " 'Atlas Shrugged' ", 1140));
+        bookList.add(new Book(" 'Rowling' ", " 'Harry Potter' ", 260));
+        bookList.add(new Book(" 'Ayn Rand1' ", " 'Abckedabra' ", 11));
+        bookList.add(new Book(" 'Ayn Rand2' ", " 'Abckedabra' ", 1140));
+        for (Book books : bookList) {
+            System.out.println(books);
+        }
 
-    System.out.println("Sort by Title -  Comparator:");
-    bookList.sort(new BookNameComparator());
-    for (Book books : bookList) {
-      System.out.println(books);
-    }
+        System.out.println("Sort by author:");
+        Collections.sort(bookList);
+        for (Book books : bookList) {
+            System.out.println(books);
+        }
 
-    System.out.println("Sort by pages Comparator:");
-    bookList.sort(new BookPagesComperator());
-    for (Book books : bookList) {
-      System.out.println(books);
+        System.out.println("Sort by Title -  Comparator:");
+        bookList.sort(new BookNameComparator());
+        for (Book books : bookList) {
+            System.out.println(books);
+        }
+
+        System.out.println("Sort by pages Comparator:");
+        bookList.sort(new BookPagesComperator());
+        for (Book books : bookList) {
+            System.out.println(books);
+        }
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println("======NEW SORT 14.04 LAMBDA");
+        bookList.sort((Book o1, Book o2) -> o1.getTitle().compareTo(o2.getTitle()));
+        for (Book books : bookList) {
+            System.out.println(books);
+        }
+//        public int compare(Book o1, Book o2) {
+//            if (!o1.getTitle().equals(o2.getTitle())) {
+//                return o1.getTitle().compareTo(o2.getTitle());
+//            }
+//            return o1.getAuthor().compareTo(o2.getAuthor());
+
+
+
     }
-  }
 }
+
 
 
 
